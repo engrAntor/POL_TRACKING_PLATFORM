b755efab5b09387d14595f3acd9c4cdf4dc26954
abrar-khan-alvi <abrar1khan2@gmail.com>
2026-04-30 00:48:03 +0600

    fixed the error1

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "POL Tracking Platform",
     description: "Inventory management and intelligent tracking",
+    icons: {
+        icon: "/assets/logo/logo.png",
+    },
 };
 
 export default function RootLayout({
